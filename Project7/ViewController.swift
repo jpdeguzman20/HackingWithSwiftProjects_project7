d8d0ae0e1270c6649a312f59555b6eb0d1fc6780
JP Deguzman <jddeguzman@ucdavis.edu>
2016-11-29 15:16:00 -0800

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Jonathan Deguzman on 11/29/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
